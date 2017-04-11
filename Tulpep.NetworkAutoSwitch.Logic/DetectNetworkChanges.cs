58b057be6835ba767dd2e75668abafc4d66c13ab
Ricardo Polo <ricardo.polo@tulpep.com>
2017-04-11 17:02:22 -0500

    Logic project filled

---
+++ Tulpep.NetworkAutoSwitch.Logic/DetectNetworkChanges.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Tulpep.NetworkAutoSwitch.Logic
+{
+    class DetectNetworkChanges
+    {
+        private DetectNetworkChanges()
+        {
+            ManageNetworkState.AnalyzeNow();
+            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(NetworkAddressChanged);
+        }
+
+        private void NetworkAddressChanged(object sender, EventArgs e)
+        {
+            ManageNetworkState.AnalyzeNow();
+        }
+
+    }
+}
---
+++ Tulpep.NetworkAutoSwitch.Logic/ManageNetworkState.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace Tulpep.NetworkAutoSwitch.Logic
+{
+    static class ManageNetworkState
+    {
+
+        private static NetworkState _networkState = new NetworkState();
+
+        public static void AnalyzeNow()
+        {
+            RefreshNetworkState();
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Wireless " + _networkState.WirelessIsUp);
+            Console.WriteLine("Wired " + _networkState.WiredIsUp);
+            if (_networkState.WirelessIsUp && _networkState.WiredIsUp)
+            {
+                ChangeNicState(_networkState.WirelessAdapters, true);
+                ChangeNicState(_networkState.WiredAdapters, false);
+            }
+            else if (!_networkState.WirelessIsUp && !_networkState.WiredIsUp)
+            {
+                ChangeNicState(_networkState.WiredAdapters, true);
+                ChangeNicState(_networkState.WiredAdapters, true);
+            }
+        }
+
+
+        private static void RefreshNetworkState()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            IEnumerable<NetworkInterface> wirelessAdapters = nics.Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Wireless80211);
+            _networkState.WirelessIsUp = wirelessAdapters.Any(x => x.OperationalStatus == OperationalStatus.Up);
+            foreach (NetworkInterface nic in wirelessAdapters)
+            {
+                _networkState.WirelessAdapters.Add(nic.Name);
+            }
+
+            IEnumerable<NetworkInterface> wiredAdapters = nics.Where(x => x.NetworkInterfaceType == NetworkInterfaceType.Ethernet);
+            _networkState.WiredIsUp = wiredAdapters.Any(x => x.OperationalStatus == OperationalStatus.Up);
+            foreach (NetworkInterface nic in wiredAdapters)
+            {
+                _networkState.WiredAdapters.Add(nic.Name);
+            }
+        }
+
+
+        private static void ChangeNicState(IEnumerable<string> interfacesNames, bool enable)
+        {
+            if (enable) foreach (string nic in interfacesNames) ChangeNicState(nic, true);
+            else foreach (string nic in interfacesNames) ChangeNicState(nic, false);
+        }
+
+        private static void ChangeNicState(string interfaceName, bool enable)
+        {
+            string arguments = "interface set interface \"" + interfaceName + "\" enable";
+            if (!enable) arguments = "interface set interface \"" + interfaceName + "\" disable";
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "netsh",
+                Arguments = arguments,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Console.WriteLine("netsh " + arguments);
+            Process process = new Process { StartInfo = processStartInfo };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+
+}
