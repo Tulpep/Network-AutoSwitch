1d46b3afe7ed8685746ba4d89c8800c5bb981856
Ricardo Polo <ricardo.polo@tulpep.com>
2017-04-11 19:04:22 -0500

    New service description

--- Tulpep.NetworkAutoSwitch.Service/ProjectInstaller.Designer.cs
+++ Tulpep.NetworkAutoSwitch.Service/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.Description = "For more information visit https://github.com/Tulpep/Network-AutoSwitch";
+            this.serviceInstaller1.Description = "For detailed information visit https://github.com/Tulpep/Network-AutoSwitch";
             this.serviceInstaller1.DisplayName = "Tulpep Network AutoSwitch Service";
             this.serviceInstaller1.ServiceName = "NetworkAutoSwitch";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
