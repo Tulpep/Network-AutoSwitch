351bd9e4aaa9b76946b650b0943a561a53fc49db
Ricardo Polo <ricardo.polo@tulpep.com>
2017-08-10 20:11:37 -0500

    Working

--- Tulpep.NetworkAutoSwitch.Service/ProjectInstaller.cs
+++ Tulpep.NetworkAutoSwitch.Service/ProjectInstaller.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
+using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Tulpep.NetworkAutoSwitch.Service
 {
@@ -17,9 +19,25 @@ namespace Tulpep.NetworkAutoSwitch.Service
 
         protected override void OnBeforeInstall(System.Collections.IDictionary savedState)
         {
-            Context.Parameters["assemblypath"] += "\" /service";
+
+            StringBuilder sbPathWIthParams = new StringBuilder(Context.Parameters["assemblypath"]);
+
+            //Wrap the existing path in quotes if it isn't already
+            if (!sbPathWIthParams[0].Equals("\""))
+            {
+                sbPathWIthParams.Insert(0, "\"");
+                sbPathWIthParams.Append("\"");
+            }
+
+            //Add desired parameters
+            sbPathWIthParams.Append(" test");
+
+            Context.Parameters["assemblypath"] = sbPathWIthParams.ToString();
+            File.WriteAllText(@"C:\log.txt", sbPathWIthParams.ToString());
             base.OnBeforeInstall(savedState);
+
         }
+
     }
 
 
