8053fdd49164f889dfca7d5dbe51f9b5c41b7b82
Daniel Vergara <daniel.vergara.echeverri@gmail.com>
2017-08-25 18:57:07 -0500

    ProxyEnabler Project

---
+++ Tulpep.NetworkAutoSwitch.ProxyEnabler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tulpep.NetworkAutoSwitch.ProxyEnabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
